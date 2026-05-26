0a5d78d3a75d21cb33a5f1c98c5fe6ce2f8a7fd9
kanha-here <kanhacoderx@gmail.com>
2026-05-26 21:05:28 +0530

    Add files via upload

---
+++ dist/server/assets/_tanstack-start-manifest_v-Bb2qYeZ-.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/ASUS/Desktop/Documind Ai/frontend/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-DfGWMZXQ.js"] }, "/": { filePath: "C:/Users/ASUS/Desktop/Documind Ai/frontend/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DBJ6Ngtv.js"] } }, clientEntry: "/assets/index-DfGWMZXQ.js" });
+export {
+  tsrStartManifest
+};
